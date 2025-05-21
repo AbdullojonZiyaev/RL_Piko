7396758d55e2bed4400e5a41c22cec9374c9bf7a
AbdullojonZiyaev <ziyaevwork@gmail.com>
2025-05-21 23:01:38 +0400

    Made more complex level and put global variable for player settings

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from config import TILE_SIZE, PLAYER_COLOR
+from config import TILE_SIZE, PLAYER_COLOR, MOVE_SPEED, JUMP_FORCE, GRAVITY, TERMINAL_VELOCITY
 
 class Player:
     def __init__(self, x, y):
@@ -12,25 +12,25 @@ class Player:
         keys = pygame.key.get_pressed()
         self.move_x = 0
         if keys[pygame.K_LEFT]:
-            self.move_x = -5
+            self.move_x = -MOVE_SPEED
         elif keys[pygame.K_RIGHT]:
-            self.move_x = 5
-        
+            self.move_x = MOVE_SPEED
+
         if keys[pygame.K_SPACE] and self.on_ground:
-            self.vel_y = -10
+            self.vel_y = -JUMP_FORCE
 
     def apply_gravity(self):
-        self.vel_y += 0.9
-        if self.vel_y > 20:  # Terminal velocity limit
-            self.vel_y = 20
+        self.vel_y += GRAVITY
+        if self.vel_y > TERMINAL_VELOCITY:
+            self.vel_y = TERMINAL_VELOCITY
 
     def handle_horizontal_collisions(self, collidables):
         self.rect.x += self.move_x
         for tile in collidables:
             if self.rect.colliderect(tile):
-                if self.move_x > 0:  # moving right
+                if self.move_x > 0:
                     self.rect.right = tile.left
-                elif self.move_x < 0:  # moving left
+                elif self.move_x < 0:
                     self.rect.left = tile.right
 
     def handle_vertical_collisions(self, collidables):
@@ -38,11 +38,11 @@ class Player:
         self.on_ground = False
         for tile in collidables:
             if self.rect.colliderect(tile):
-                if self.vel_y > 0:  # falling down
+                if self.vel_y > 0:
                     self.rect.bottom = tile.top
                     self.vel_y = 0
                     self.on_ground = True
-                elif self.vel_y < 0:  # moving up
+                elif self.vel_y < 0:
                     self.rect.top = tile.bottom
                     self.vel_y = 0
 
